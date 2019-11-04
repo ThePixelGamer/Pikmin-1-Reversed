86b081d959ca7fee86b11c89030064c1cdee0baf
JohnFiddleystein <patrickantonisawesome@gmail.com>
2019-11-04 01:09:08 +0000

    :ambulance: woops, actually fix VtxMatrix this time

--- sysCore/source/Model/VtxMatrix.cpp
+++ sysCore/source/Model/VtxMatrix.cpp
@@ -3,11 +3,21 @@
 void VtxMatrix::read(RandomAccessStream& stream)
 {
     const short idx = stream.readShort();
-    this->m_index = (this->m_partiallyWeighted = idx >= 0) ? idx : -idx - 1;
+    this->m_partiallyWeighted = idx >= 0;
+    if (this->m_partiallyWeighted)
+        this->m_index = idx;
+    else
+        this->m_index = -idx - 1;
 }
 
 void VtxMatrix::write(RandomAccessStream& stream)
 {
-    // Basically an if statement
-    this->m_partiallyWeighted ? stream.writeShort(this->m_index) : stream.writeShort(-(this->m_index + 1));
+    if (this->m_partiallyWeighted)
+    {
+        stream.writeShort(this->m_index);
+    }
+    else
+    {
+        stream.writeShort(-(this->m_index + 1));
+    }
 }
